3df92a8c712f350f661d9c4a50af449184431cd6
Mehmet Tamtürk <mehmettamturk@gmail.com>
2016-06-14 13:20:40 +0300

    done

--- app/src/app.js
+++ app/src/app.js
@@ -1,15 +1,4 @@
 'use strict';
 
-var microserviceKit = require('altayer/lib/microservicekit');
-
-microserviceKit
-    .init()
-    .then(() => {
-        console.info('Microservicekit is initalized.');
-
-        const app = require('altayer/app/index');
-        app.listenBound();
-    })
-    .catch((err) => {
-        console.error("Cannot initalize microservicekit!", err);
-    });
+const app = require('altayer/app/index');
+app.listenBound();
--- app/src/components/search/controller.js
+++ app/src/components/search/controller.js
@@ -1,23 +1,20 @@
 "use strict";
 
+const config = require('config');
 const request = require('request');
 const router = require('express').Router();
-
-var microserviceKit = require('altayer/lib/microservicekit');
-var searchQueue = microserviceKit.amqpKit.getQueue('search');
 
 /**
  * Get Search
  */
 router.get('/optimized-search', function(req, res, next) {
-    searchQueue
-        .sendEvent('SEARCH_KEYWORD', { keyword: req.query.keyword }, { persistent: true })
-        .then((searchResults) => {
-            res.send(searchResults);
-        })
-        .catch((err) => {
+    var keyword = req.query.keyword;
+    request('http://test.sonsuzdongu.com/query.php?keyword=' + keyword, function (error, response, body) {
+        if (!error && response.statusCode == 200)
+            res.send(body);
+        else
             res.status(500).send(err);
-        });
+    });
 });
 
 module.exports = router;
